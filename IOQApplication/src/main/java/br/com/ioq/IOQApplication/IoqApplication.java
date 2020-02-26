cfd6b10196528633019e193864c856b75b4ef4bd
LintonJr <lintonjr@hotmail.com>
2020-02-26 13:14:46 -0400

    commit simple application

---
+++ IOQApplication/src/main/java/br/com/ioq/IOQApplication/IoqApplication.java
@@ -0,0 +1,13 @@
+package br.com.ioq.IOQApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoqApplication.class, args);
+	}
+
+}
